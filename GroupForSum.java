7a0e972ea1c30c450f462052a3eec1d61ac37444
KishorNanaware <132504002+KishorNanaware@users.noreply.github.com>
2024-11-11 15:01:53 +0530

    Create pair of number having Sum equal to 9 from list of integer
    With Help stream api

--- GroupForSum.java
+++ GroupForSum.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class GroupForNumber {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		
         List<Integer> inputList = Arrays.asList(1,8,2,6,7,2,5,7);
         groupNumberforSum(inputList,9);
